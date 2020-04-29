7edb9429e1395f7cef5a56c7cfa7275f7711f8d6
gliconesapiens <64334166+gliconesapiens@users.noreply.github.com>
2020-04-29 16:38:58 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
  function random(){
 
-document.getElementById("one").innerHTML = Math.floor(Math.random() * 10);
+var words = [];
+var adj = [];
+
+$.get('/glicone/nouns.txt', function(response) {
+  console.log('starting response noun')
+  words = response.split('\n');
+  console.log(words[0])
+  document.getElementById("one").innerHTML = words[Math.floor(Math.random() * words.length)];
+})
+ 
+$.get('/glicone/aa.txt', function(response) {
+  console.log('starting response adj')
+  adj = response.split('\n');
+  console.log(words[0])
+  document.getElementById("two").innerHTML = adj[Math.floor(Math.random() * adj.length)];
+})
+  
 }
